262e9dfdb45d1dbdee2fe8833cda51a2d5f0e672
Rabie AL-Ashmali <rabiealashmali@gmail.com>
2022-11-02 16:49:32 +0000

    added the shows list - level 500

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ function removeAllChildeNodes(parent) {
 function setup() {
   fetch("https://api.tvmaze.com/shows")
     .then((response) => response.json())
-    .then((allShows) => showsDropDown(allShows));
+    .then((allShows) => {showsDropDown(allShows); makePageForShows(allShows)});
+
 }
 
 function getEpisodes() {
@@ -41,10 +42,18 @@ function makePageForEpisodes(episodeList) {
   });
 
   displayingNumOfEp.innerHTML = `Displaying ${episodeList.length}/${allEpisodes.length}`;
+  searchFun(episodeList);
 }
 
+// function homeBtn(){
+//   const homeBtn = document.querySelector("button"); 
+//   homeBtn.addEventListener("click", (e) => {
+//     makePageForShows(allShows);
+//   })
+// }
 // Search function
-function searchFun() {
+function searchFun(allEpisodes) {
+
   const searchInput = document.querySelector("[data-search]");
   searchInput.addEventListener("input", (e) => {
     let value = e.target.value.toLowerCase();
@@ -63,7 +72,6 @@ function searchFun() {
   });
 }
 
-searchFun();
 
 // Episode drop-down menu
 const numberFormat = (episodeCode) => {
@@ -71,7 +79,9 @@ const numberFormat = (episodeCode) => {
 };
 
 function dropDownAllEpisodes() {
+  
   let dropDownEP = document.getElementById("episodesDropDown");
+  dropDownEP.innerHTML = "";
   for (let i = 0; i < allEpisodes.length; i++) {
     let options = document.createElement("option");
     options.setAttribute("value", allEpisodes[i].id);
@@ -119,14 +129,76 @@ function showsDropDown(showList) {
 function displayTheShow(e) {
   removeAllChildeNodes(rootElem);
   showId = e.target.value;
-  console.log(showId);
-
   fetch("https://api.tvmaze.com/shows/" + showId + "/episodes")
     .then((response) => response.json())
     .then((data) => (allEpisodes = data))
     .then(() => getEpisodes());
 }
 
-// dropDownAllEpisodes();
+function makePageForShows(showsList) {
+  rootElem.innerHTML = ""; 
+  showsList.forEach((show) => {
+    let cardEl = document.createElement("div");
+    let titleEl = document.createElement("h2");
+    let imageEl = document.createElement("img");
+    let summaryEl = document.createElement("div");
+    let extraInfoEl = document.createElement("div");
+    let rateEl = document.createElement("div");
+    let genresEl = document.createElement("div");
+    let statusEl = document.createElement("div");
+    let runTimeEl = document.createElement("div");
+    cardEl.className = "card";
+    titleEl.className = "title";
+    imageEl.className = "img";
+    summaryEl.className = "summary";
+    extraInfoEl.className = "extra-info";
+    rateEl.className = "rate"; 
+    genresEl.className = "genres";
+    statusEl.className = "status"; 
+    runTimeEl.className = "run-time"; 
+
+    titleEl.innerText = show.name;
+    imageEl.setAttribute("src", show.image.medium);
+    summaryEl.innerHTML = show.summary;
+    rateEl.innerHTML = `Rated: ${show.rating.average}`;
+    genresEl.innerHTML = `Genres: ${show.genres}`; 
+    statusEl.innerHTML = `Status: ${show.status}`
+    runTimeEl.innerHTML = `Runtime: ${show.runtime}`;
+    extraInfoEl.append(rateEl, genresEl, statusEl, runTimeEl); 
+    cardEl.append(titleEl, imageEl, summaryEl, extraInfoEl);
+    rootElem.append(cardEl);
+
+    //styling content
+    
+  });
+
+  searchShowFun(showsList); 
+
+
+
+  // displayingNumOfEp.innerHTML = `Displaying ${episodeList.length}/${allEpisodes.length}`;
+}
+
+function searchShowFun(allEpisodes) {
+  const searchInput = document.querySelector("[data-search]");
+  searchInput.addEventListener("input", (e) => {
+    let value = e.target.value.toLowerCase();
+    let isVisible = allEpisodes.filter((user) => {
+      return (
+        user.name.toLowerCase().includes(value) ||
+        user.summary.toLowerCase().includes(value)
+      );
+
+      //  user.element.classList.toggle("hide", !isVisible);
+    });
+
+    rootElem.innerHTML = "";
+    makePageForShows(isVisible);
+    // showDropDown(isVisible);
+    displayingNumOfEp.innerHTML = `Displaying ${isVisible.length}/${allEpisodes.length}`;
+  });
+}
+
+
 
 window.onload = setup;
